a618ca5da233867b5cfa2c40ac9cff2e059f39de
Nikolay Tsonev <nikolay.tsonev@telerik.com>
2016-04-14 11:10:48 +0300

    add navigation transition

--- app/page2.ts
+++ app/page2.ts
@@ -1,6 +1,16 @@
 import {topmost} from "ui/frame";
 
 export function back(){
+    //crash on return to the main page
+    //topmost().transition = { name: "fade" };
+    //topmost().transition = { name: "flip" };
+    
+    //works only twice 
+    //topmost().transition = { name: "slideRight" };
+    
+    //works only twice 
+    topmost().transition = { name: "slideBottom" }
+    
     topmost().navigate({
         moduleName:"main-page", 
         clearHistory:true
